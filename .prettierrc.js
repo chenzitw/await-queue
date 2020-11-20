af1acd5eed794e46cd207f8ab604015ac3b8d78a
ChenZi <chenzi@ubun.tw>
2020-11-20 17:50:00 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  fluid: false,
+};
---
+++ .size-limit.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    path: 'dist/await-queue.cjs.production.min.js',
+    limit: '10 KB',
+  },
+  {
+    path: 'dist/await-queue.esm.js',
+    limit: '10 KB',
+  },
+];
